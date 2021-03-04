daa5dcd1b41f53c49aec4fd1cd35359d92ca271c
Milo Silva <milo.silva234@gmail.com>
2021-03-04 19:01:54 +0100

    Clear redundant functions from code base

--- components/TinderCard.js
+++ components/TinderCard.js
@@ -4,19 +4,10 @@ import { View, PanResponder, Text, Dimensions } from "react-native";
 import { useSpring, animated, interpolate, config } from "react-spring";
 
 const settings = {
-  snapBackDuration: 300,
+  friction: 50,
+  tension: 800,
   maxTilt: 5,
-  bouncePower: 0.2,
   swipeThreshold: 1.5, // need to update this threshold for RN (1.5 seems reasonable...?)
-};
-
-const getElementSize = (element) => {
-  const elementStyles = window.getComputedStyle(element);
-  const widthString = elementStyles.getPropertyValue("width");
-  const width = Number(widthString.split("px")[0]);
-  const heightString = elementStyles.getPropertyValue("height");
-  const height = Number(heightString.split("px")[0]);
-  return { x: width, y: height };
 };
 
 const pythagoras = (x, y) => {
@@ -33,27 +24,10 @@ const animateOut = async (gesture, set, easeIn = false) => {
   const time = diagonal / velocity;
   const multiplier = diagonal / velocity; // needs to be adjusted as current way velocity is measured is confusing.
   console.log("multiplier:", multiplier);
-
-  // const translateString = translationString(
-  //   speed.x * multiplier + startPos.x,
-  //   -speed.y * multiplier + startPos.y
-  // );
-  const rotationPower = 200;
-
-  // if (easeIn) {
-  //   element.style.transition = "ease " + time + "s";
-  // } else {
-  //   element.style.transition = "ease-out " + time + "s";
-  // }
-
-  // if (getRotation(element) === 0) {
-  //   rotateString = rotationString((Math.random() - 0.5) * rotationPower);
-  // } else if (getRotation(element) > 0) {
-  //   rotateString = rotationString((Math.random() * rotationPower) / 2 + getRotation(element));
-  // } else {
-  //   rotateString = rotationString(((Math.random() - 1) * rotationPower) / 2 + getRotation(element));
-  // }
+  // calculate rotation of the element
+  // any way to read the current rotation value?
   console.log("velocity:", velocity);
+  // this part needs to be improved.
   set({
     x: (multiplier * gesture.vx) / 2,
     y: (multiplier * gesture.vy) / 2,
@@ -61,27 +35,17 @@ const animateOut = async (gesture, set, easeIn = false) => {
   });
   // element.style.transform = translateString + rotateString;
 
-  await await new Promise((resolve) => setTimeout(() => resolve(), 1000));
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      set({ x: 0, y: 0 });
+      resolve();
+    }, 1000)
+  );
 };
 
 const animateBack = (set) => {
   // translate back to the initial position
   set({ x: 0, y: 0 });
-  // element.style.transition = settings.snapBackDuration + "ms";
-  // const startingPoint = getTranslate(element);
-  // const translation = translationString(
-  //   startingPoint.x * -settings.bouncePower,
-  //   startingPoint.y * -settings.bouncePower
-  // );
-  // const rotation = rotationString(getRotation(element) * -settings.bouncePower);
-  // element.style.transform = translation + rotation;
-  // setTimeout(() => {
-  //   element.style.transform = "none";
-  // }, settings.snapBackDuration * 0.75);
-
-  // setTimeout(() => {
-  //   element.style.transition = "10ms";
-  // }, settings.snapBackDuration);
 };
 
 const getSwipeDirection = (speed) => {
@@ -90,24 +54,6 @@ const getSwipeDirection = (speed) => {
   } else {
     return speed.y > 0 ? "up" : "down";
   }
-};
-
-const dragableTouchmove = (coordinates, element, offset, lastLocation) => {
-  const pos = { x: coordinates.x + offset.x, y: coordinates.y + offset.y };
-  const newLocation = { x: pos.x, y: pos.y, time: new Date().getTime() };
-  const translation = translationString(pos.x, pos.y);
-  const rotCalc = calcSpeed(lastLocation, newLocation).x / 1000;
-  const rotation = rotationString(rotCalc * settings.maxTilt);
-  element.style.transform = translation + rotation;
-  return newLocation;
-};
-
-const touchCoordinatesFromEvent = (e) => {
-  return { x: e.nativeEvent.locationX, y: e.nativeEvent.locationY };
-};
-
-const mouseCoordinatesFromEvent = (e) => {
-  return { x: e.clientX, y: e.clientY };
 };
 
 // must be created outside of the TinderCard forwardRef
@@ -124,17 +70,8 @@ const TinderCard = React.forwardRef(
     const [{ x, y }, set] = useSpring(() => ({
       x: 0,
       y: 0,
-      config: { friction: 50, tension: 800 },
+      config: { friction: settings.friction, tension: settings.tension },
     }));
-    const element = React.useRef();
-    // const { x, y, size } = useSpring({
-    //   x: down ? delta[0] : 0,
-    //   bg: `linear-gradient(120deg, ${
-    //     delta[0] < 0 ? "#f093fb 0%, #f5576c" : "#96fbc4 0%, #f9f586"
-    //   } 100%)`,
-    //   size: down ? 1.1 : 1,
-    //   immediate: (name) => down && name === "x",
-    // });
 
     React.useImperativeHandle(ref, () => ({
       async swipe(dir = "right") {
@@ -194,7 +131,7 @@ const TinderCard = React.forwardRef(
           onPanResponderGrant: (evt, gestureState) => {
             // The gesture has started.
             // Probably wont need this anymore as postion i relative to swipe!
-            console.log("press in");
+            console.log("touch down!");
           },
           onPanResponderMove: (evt, gestureState) => {
             // The most recent move distance is gestureState.move{X,Y}
@@ -216,27 +153,13 @@ const TinderCard = React.forwardRef(
           onPanResponderRelease: (evt, gestureState) => {
             // The user has released all touches while this view is the
             // responder. This typically means a gesture has succeeded
-            console.log("press out");
+            console.log("touch up");
             // enable
             handleSwipeReleased(set, gestureState);
           },
         }),
       []
     );
-
-    //   TODO Implement these events! using the panResponder!
-
-    // element.current.addEventListener(('touchmove'), (ev) => {
-    //   ev.preventDefault()
-    //   const newLocation = dragableTouchmove(touchCoordinatesFromEvent(ev), element.current, offset, lastLocation)
-    //   speed = calcSpeed(lastLocation, newLocation)
-    //   lastLocation = newLocation
-    // })
-
-    // element.current.addEventListener(('touchend'), (ev) => {
-    //   ev.preventDefault()
-    //   handleSwipeReleased(element.current, speed)
-    // })
     return (
       <AnimatedView
         {...panResponder.panHandlers}
